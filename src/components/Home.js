6c8f9f2048719b8a95eecb146f2b9ba41f27fef6
Lavesh Gaurav <lavesh.g96@gmail.com>
2020-01-22 22:34:02 +0530

    Animation Activated for Desktop

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,23 @@ import { gsap } from 'gsap';
 import Logo from '../assets/Elabs logo black.png';
 import { Link } from 'react-router-dom';
 function Home() {
-  // useEffect(() => {
-  //   gsap.from('.one', { x: -1000, duration: 1.2 });
-  //   gsap.from('.two', { y: -1000, duration: 1.2, delay: 1.2 });
-  // });
+  useEffect(() => {
+    if (window.innerWidth >= 768) {
+      gsap.from('.one', { flex: 0, x: -1000, duration: 1.2 });
+      gsap.from('.two', { flex: 5, duration: 1.2, delay: 1.6 });
+      gsap.from('.overflow', { y: -1000, duration: 1.2, delay: 2.4 });
+      gsap.from('.h6', { y: -100, duration: 1.2, delay: 2.4, opacity: 0 });
+      gsap.from('.h1', { y: -100, duration: 1.2, delay: 3.6, opacity: 0 });
+      gsap.from('button', { x: -400, duration: 1.2, delay: 3.6, opacity: 0 });
+    }
+  });
   return (
     <div>
       <div className='wrapper'>
         <div className='one'>
           <div className='heading'>
-            <h6>Become a part of</h6>
-            <h1>E LABS</h1>
+            <h6 className='h6'>Become a part of</h6>
+            <h1 className='h1'>E LABS</h1>
             <button>
               <Link to='/register'>REGISTER</Link>
             </button>
